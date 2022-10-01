046151ea2e7e06a4d2cbb848a32e1a80ef65eebd
Nelson Lu <nelsonlu3@gmail.com>
2022-10-01 17:34:23 -0400

    Update main.py
    test push. VS Code broke

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import csv
 from numpy import product
 import pandas as pd
 
-
+print("test push")
 
 product = pd.read_csv('dummyProduct.csv',index_col=0)
 recipe = pd.read_csv('dummyRecipe.csv',index_col=0).transpose()
