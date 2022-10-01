62ca69cb81088bebc1244fba61b9598155121877
lpfalz <102377622+lpfalz@users.noreply.github.com>
2022-10-01 17:04:07 -0400

    used df created

--- main.py
+++ main.py
@@ -8,5 +8,13 @@ product = pd.read_csv('dummyProduct.csv',index_col=0).transpose()
 recipe = pd.read_csv('dummyRecipe.csv',index_col=0).transpose()
 sold = pd.read_csv('dummySold.csv',index_col=0).transpose()
 rec=list(recipe.columns)
-print(rec)
+prod=sold["burger"]
+#recipe["burger"]*=prod[0]
+used=pd.DataFrame()
+for i in rec:
+    prod=sold[i]
+    used[i]=recipe[i]*prod[0]
+
+print(used)
+
 
