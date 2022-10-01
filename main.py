ec307530d5aa30e4d92787a0a43defc719053987
lpfalz <102377622+lpfalz@users.noreply.github.com>
2022-10-01 17:19:57 -0400

    calculated difference

--- main.py
+++ main.py
@@ -4,17 +4,13 @@ import pandas as pd
 
 
 
-product = pd.read_csv('dummyProduct.csv',index_col=0).transpose()
+product = pd.read_csv('dummyProduct.csv',index_col=0)
 recipe = pd.read_csv('dummyRecipe.csv',index_col=0).transpose()
 sold = pd.read_csv('dummySold.csv',index_col=0).transpose()
 rec=list(recipe.columns)
-prod=sold["burger"]
-#recipe["burger"]*=prod[0]
 used=pd.DataFrame()
 for i in rec:
     prod=sold[i]
     used[i]=recipe[i]*prod[0]
 used["sum"] = used.sum(axis=1)
-print(used)
-
-
+remaining=product["Count"]-used["sum"]
