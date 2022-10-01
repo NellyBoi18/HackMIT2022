34af46f65df340b5bfce627d31a70fe2c2b3deb5
lpfalz <102377622+lpfalz@users.noreply.github.com>
2022-10-01 16:51:57 -0400

    transposed

--- main.py
+++ main.py
@@ -4,10 +4,9 @@ import pandas as pd
 
 
 
-product = pd.read_csv('dummyProduct.csv',index_col=0)
-recipe = pd.read_csv('dummyRecipe.csv',index_col=0)
-sold = pd.read_csv('dummySold.csv',index_col=0)
+product = pd.read_csv('dummyProduct.csv',index_col=0).transpose()
+recipe = pd.read_csv('dummyRecipe.csv',index_col=0).transpose()
+sold = pd.read_csv('dummySold.csv',index_col=0).transpose()
 
 
-p=sold.loc["burger"]
-print(p)
+
