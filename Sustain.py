8aaa3d3316c939b1074119fc405a46cc47ce0702
Evelyn Martinez <101659674+eevelxnn@users.noreply.github.com>
2022-10-01 16:07:58 -0400

    f

--- Sustain.py
+++ Sustain.py
@@ -3,4 +3,10 @@ from numpy import product
 import pandas as pd
 
 product = pd.read_csv('dummyProduct.csv')
-print(product.to_string())
+print(product)
+
+recipe = pd.read_csv('dummyRecipe.csv')
+print(recipe)
+
+sold = pd.read_csv('dummySold.csv')
+print(sold)
