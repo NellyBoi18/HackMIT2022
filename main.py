51987abd7a1179c7994b69ef47baa9fcc92c6ae9
lpfalz <102377622+lpfalz@users.noreply.github.com>
2022-10-01 16:54:09 -0400

    list of rec

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ import pandas as pd
 product = pd.read_csv('dummyProduct.csv',index_col=0).transpose()
 recipe = pd.read_csv('dummyRecipe.csv',index_col=0).transpose()
 sold = pd.read_csv('dummySold.csv',index_col=0).transpose()
-
-
+rec=list(recipe.columns)
+print(rec)
 
