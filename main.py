9f755fd8617e5988df3cdb6897bbd60d96c578b7
lpfalz <102377622+lpfalz@users.noreply.github.com>
2022-10-01 19:25:35 -0400

    fixed function in main

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from numpy import product
 import pandas as pd
+from sympy import re
 
-product = pd.read_csv('dummyProduct.csv',index_col=0)
-recipe = pd.read_csv('dummyRecipe.csv',index_col=0).transpose()
-sold = pd.read_csv('dummySold.csv',index_col=0).transpose()
-rec=list(recipe.columns)
-used=pd.DataFrame()
+def calculating(inventory,recipe,sold):
+    inventoryDF = pd.read_csv(inventory,index_col=0)
+    recipeDF = pd.read_csv(recipe,index_col=0).transpose()
+    soldDF = pd.read_csv(sold,index_col=0).transpose()
+    rec=list(recipeDF.columns)
+    used=pd.DataFrame()
+    for i in rec:
+        prod=soldDF[i]
+        used[i]=recipeDF[i]*prod[0]
+    used["sum"] = used.sum(axis=1)
+    remaining=pd.DataFrame()
+    remaining["Food_Waste"]=inventoryDF["Count"]-used["sum"]
+    remaining["Cost_of_Waste"]=remaining["Food_Waste"]*inventoryDF["Unit Price"]
+    return remaining
 
-for i in rec:
-    prod=sold[i]
-    used[i]=recipe[i]*prod[0]
-used["sum"] = used.sum(axis=1)
-remaining=product["Count"]-used["sum"]
-print(remaining)
+print(calculating("dummyProduct.csv","dummyRecipe.csv","dummySold.csv"))
 
