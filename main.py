4313830f4498181e455e5c9db5792e8ed03f739a
Evelyn Martinez <101659674+eevelxnn@users.noreply.github.com>
2022-10-01 16:50:45 -0400

    change

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import csv
 from numpy import product
 import pandas as pd
 
@@ -10,14 +9,4 @@ print(recipe)
 
 sold = pd.read_csv('dummySold.csv')
 print(sold)
-
-with open('dummySold.csv') as file_obj:
-      
-    # Create reader object by passing the file 
-    # object to reader method
-    reader_obj = csv.reader(file_obj)
-      
-    # Iterate over each row in the csv 
-    # file using reader object
-    for row in reader_obj:
-        print(row)
+sold.columns
